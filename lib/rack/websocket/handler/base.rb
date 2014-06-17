bb3d7726bbb97be76394a0886ea84d058e9f3873
Damir Svrtan <damir.svrtan@gmail.com>
2014-06-17 13:19:44 +0200

    Set @env instance variable on parent.

--- lib/rack/websocket/handler/base.rb
+++ lib/rack/websocket/handler/base.rb
@@ -3,17 +3,36 @@ module Rack
     module Handler
       class Base
                 
-        autoload :Connection,     "#{ROOT_PATH}/websocket/handler/base/connection"
+        autoload :Connection, "#{ROOT_PATH}/websocket/handler/base/connection"
 
-        def on_open; @parent.on_open(@env); end # Fired when a client is connected.
-        def on_message(msg); @parent.on_message(@env, msg); end # Fired when a message from a client is received.
-        def on_close; @parent.on_close(@env); end # Fired when a client is disconnected.
-        def on_error(error); @parent.on_error(@env, error); end # Fired when error occurs.
+        def on_open
+          set_env_instance_variable
+          @parent.on_open(@env)
+        end # Fired when a client is connected.
+
+        def on_message(msg)
+          set_env_instance_variable
+          @parent.on_message(@env, msg)
+        end # Fired when a message from a client is received.
+
+        def on_close
+          set_env_instance_variable
+          @parent.on_close(@env)
+        end # Fired when a client is disconnected.
+
+        def on_error(error)
+          set_env_instance_variable
+          @parent.on_error(@env, error)
+        end # Fired when error occurs.
 
         # Set application as parent and forward options
         def initialize(parent, options = {})
           @parent = parent
           @options = options[:backend] || {}
+        end
+
+        def set_env_instance_variable
+          @parent.instance_variable_set("@env", @env)
         end
 
         # Implemented in subclass
